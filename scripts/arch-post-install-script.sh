2de9ca6632a3a498866ee7c1ccd5d6dc6e9c4c8e
Jofr3 <jofrescari@gmail.com>
2024-02-13 11:30:32 +0000

    update install scripts

--- scripts/arch-post-install-script.sh
+++ scripts/arch-post-install-script.sh
@@ -1,47 +1,21 @@
 #!/bin/bash
 
 # Create basic folders
-mkdir ~/Projects ~/Personal ~/Desktop ~/Downloads ~/.ssh ~/.ssh/keys
+mkdir ~/Projects ~/Personal ~/Desktop ~/Downloads ~/.ssh
 
 # Install packages
-sudo pacman -S --noconfirm npm chromium docker docker-compose git kitty neofetch fzf ripgrep ttf-jetbrains-mono tree zsh nautilus xf86-input-synaptics neovim alsa-utils sof-firmware brightnessctl ntfs-3g unzip bluez bluez-utils lsof tmux xclip gpick zoxide exa bat lazygit man-db man-pages picom bluez
-
-
-# Install Aura
-git clone https://aur.archlinux.org/aura-bin.git
-cd aura-bin
-makepkg
-sudo pacman -U --noconfirm *.zst
-cd ~
-sudo rm -r aura-bin
-
-# Install AUR packages
-sudo aura -A --noconfirm google-chrome bluetuith
-
-# Install Ohmyzsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# Install Ohmyzsh plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-rm ~/.zshrc
-rm .bash*
+sudo pacman -S --noconfirm npm chromium docker docker-compose git kitty neofetch ripgrep ttf-jetbrains-mono tree neovim alsa-utils sof-firmware brightnessctl ntfs-3g unzip bluez bluez-utils lsof tmux xclip gpick zoxide man-db man-pages picom bluez ly
 
 # Cloning dotfiles
 git clone git@github.com:Jofr3/.dotfiles.git ~/Desktop/.dotfiles
 
 # Linking dotfiles
-ln -s ~/Desktop/.dotfiles/qtile ~/.config/qtile
+ln -s ~/Desktop/.dotfiles/awesome ~/.config/awesome
 ln -s ~/Desktop/.dotfiles/nvim ~/.config/nvim
 ln -s ~/Desktop/.dotfiles/kitty ~/.config/kitty
-ln -s ~/Desktop/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
 ln -s ~/Desktop/.dotfiles/picom ~/.config/picom
-ln -s ~/Desktop/.dotfiles/zshrc/.zshrc ~/.zshrc
-ln -s ~/Desktop/.dotfiles/zshrc/.zshenv ~/.zshenv    
+ln -s ~/Desktop/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
 ls -s ~/Desktop/.dotfiles/xorg/.xinitrc ~/.xinitrc
-
-sudo chmod +x ~/.xinitrc
 
 # Fix audio bug
 ln -s ~/Desktop/.dotfiles/other/default.pa /etc/pulse/default.pa
@@ -49,20 +23,11 @@ ln -s ~/Desktop/.dotfiles/other/default.pa /etc/pulse/default.pa
 # Fix trackpad light touch
 sudo ln -s ~/Desktop/.dotfiles/xorg/70-synaptics.conf /etc/X11/xorg.conf.d/70-synaptics.conf
 
-# Install nerd font icons
-mkdir icons
-cd icons
-curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/NerdFontsSymbolsOnly.zip
-unzip NerdFontsSymbolsOnly.zip
-mkdir ~/.local/share/fonts/
-mv SymbolsNerdFontMono-Regular.ttf ~/.local/share/fonts/
-mv SymbolsNerdFont-Regular.ttf ~/.local/share/fonts/
-sudo fc-cache -fv
-cd ..
-rm -r icons
-
 # Give execute permissions to scripts
+sudo chmod +x ~/.xinitrc
 sudo chmod +x ~/Desktop/.dotfiles/scripts/utils/*
 sudo chmod +x ~/Desktop/.dotfiles/scripts/tools/*
 
 sudo systemctl enable bluetooth.service
+sudo systemctl enable ly.service
+
--- scripts/ubuntu-post-install-script.sh
+++ scripts/ubuntu-post-install-script.sh
@@ -8,30 +8,18 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 
 # Install packages
-sudo apt install git npm python3-pip zsh kitty neofetch fzf ripgrep fonts-jetbrains-mono tree unzip lsof tmux xclip zoxide exa network-manager xinit pipewire pipewire-audio default-jdk gradle -y
+sudo apt install git npm cargo python3-pip kitty neofetch fzf ripgrep fonts-jetbrains-mono tree unzip lsof tmux xclip zoxide network-manager xinit pipewire pipewire-audio default-jdk gradle awesome -y
 
 # Install snap packages
 sudo snap install nvim --classic
 sudo snap install chromium
 
+# Install nushell
+cargo install nu
+echo -e "" >> example.txt
+
 # Connect to wifi
 nmcli device wifi connect Vera_C326AB password cab6533559
-
-# Install google-chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-sudo apt -f install
-sudo rm -r google-chrome-stable_current_amd64.deb
-
-# Install ohmyzsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# Install Ohmyzsh plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-rm ~/.zshrc
-rm .bash*
 
 # Install nerd-font
 git clone --filter=blob:none --sparse https://github.com/ryanoasis/nerd-fonts.git
@@ -45,13 +33,12 @@ sudo fc-cache -f
 git clone https://github.com/Jofr3/.dotfiles ~/Desktop/.dotfiles
 
 # Link dotfiles
-ln -s ~/Desktop/.dotfiles/qtile ~/.config/qtile
 ln -s ~/Desktop/.dotfiles/nvim ~/.config/nvim
+ln -s ~/Desktop/.dotfiles/awesome ~/.config/awesome
+ln -s ~/Desktop/.dotfiles/nushell ~/.config/nushell
 ln -s ~/Desktop/.dotfiles/kitty ~/.config/kitty
 ln -s ~/Desktop/.dotfiles/picom ~/.config/picom
 ln -s ~/Desktop/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
-ln -s ~/Desktop/.dotfiles/zshrc/.zshrc ~/.zshrc
-ln -s ~/Desktop/.dotfiles/zshrc/.zshenv ~/.zshenv    
 ls -s ~/Desktop/.dotfiles/xorg/.xinitrc ~/.xinitrc
 
 # Give execute permissions to scripts

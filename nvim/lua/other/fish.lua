edaccae22e77dcd3245601a9284608af0871a912
Jofr3 <jofrescari@gmail.com>
2024-04-30 16:13:16 +0200

    tmux + fish

---
+++ nvim/lua/other/fish.lua
@@ -0,0 +1,6 @@
+vim.keymap.set("n", "<A-!>", "<cmd>silent! !switch_tmux_session 1<CR>", { silent = true })
+vim.keymap.set("n", "<A-@>", "<cmd>silent! !switch_tmux_session 2<CR>", { silent = true })
+vim.keymap.set("n", "<A-#>", "<cmd>silent! !switch_tmux_session 3<CR>", { silent = true })
+vim.keymap.set("n", "<A-$>", "<cmd>silent! !switch_tmux_session 4<CR>", { silent = true })
+vim.keymap.set("n", "<A-%>", "<cmd>silent! !switch_tmux_session 5<CR>", { silent = true })
+vim.keymap.set("n", "<A-^>", "<cmd>silent! !switch_tmux_session 6<CR>", { silent = true })

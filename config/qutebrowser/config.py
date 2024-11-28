6fc953785ce5607f89b6744c6e8cec8db185256e
Jofr3 <jofrescai@gmail.com>
2024-11-28 20:58:40 +0100

    qutebrowser config

---
+++ config/qutebrowser/config.py
@@ -0,0 +1,8 @@
+c.url.default_page = "https://google.com";
+c.url.start_pages = [ "https://google.com" ];
+c.auto_save.session = True;
+c.completion.use_best_match = True;
+c.scrolling.smooth = True;
+c.url.searchengines = { 'DEFAULT':  'https://google.com/search?q={}' }
+
+config.load_autoconfig();

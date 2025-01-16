62bb2ff8b27858fad838c89662eda871bde9f7fe
Jofr3 <jofrescai@gmail.com>
2025-01-16 19:48:49 +0100

    fix treesitter

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,3 +1,4 @@
 require('config.opts')
 require('config.lazy')
 require('config.maps')
+require('config.cmds')
--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,13 +1,12 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   enabled = true,
-  lazy = false,
-  config = function()
-    require('nvim-treesitter').setup({
-      ensure_installed = { "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "json", "nix" },
-      highlight = {
-        enable = true
-      }
-    })
-  end
+  -- lazy = false,
+  opts = {
+    ensure_installed = { "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "json", "nix" },
+    sync_install = true,
+    highlight = {
+      enable = true
+    }
+  }
 }

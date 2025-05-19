a22d621278500b500332b6f9ce9f472108c3ee42
Jofr3 <jofrescari@gmail.com>
2025-05-19 22:21:53 +0200

    needle

--- config/nvim/lua/plugins/needle.lua
+++ config/nvim/lua/plugins/needle.lua
@@ -1,7 +1,19 @@
 return {
-    dir = "~/projects/needle",
-    lazy = false,
-    config = function()
-        require("needle").setup()
-    end
+	"Jofr3/needle",
+  enabled = true,
+	lazy = false,
+	config = function()
+		require("needle").setup()
+	end,
+	keys = {
+		{ mode = "n", "<D-q>", "<cmd>lua require('needle.mark').jump_to_mark(1)<cr>" },
+		{ mode = "n", "<D-w>", "<cmd>lua require('needle.mark').jump_to_mark(2)<cr>" },
+		{ mode = "n", "<D-e>", "<cmd>lua require('needle.mark').jump_to_mark(3)<cr>" },
+		{ mode = "n", "<D-r>", "<cmd>lua require('needle.mark').jump_to_mark(4)<cr>" },
+		{ mode = "n", "<D-t>", "<cmd>lua require('needle.mark').jump_to_mark(5)<cr>" },
+		{ mode = "n", "<D-y>", "<cmd>lua require('needle.mark').jump_to_mark(6)<cr>" },
+
+		{ mode = "n", "<D-]>", "<cmd>lua require('needle.mark').jump_to_next()<cr>" },
+		{ mode = "n", "<D-[>", "<cmd>lua require('needle.mark').jump_to_prev()<cr>" },
+	}
 }

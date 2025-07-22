9062c6bb842646e73bcca61ba150b464ced24582
Jofr3 <jofrescari@gmail.com>
2025-07-22 15:01:07 +0200

    telescope config

--- config/nvim/lua/plugins/fzf.lua
+++ config/nvim/lua/plugins/fzf.lua
@@ -1,6 +1,6 @@
 return {
 	"ibhagwan/fzf-lua",
-	enabled = true,
+	enabled = false,
 	lazy = false,
 	opts = {
 		fzf_bin = "sk",
--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
 	"nvim-telescope/telescope.nvim",
-	enabled = false,
+	enabled = true,
 	lazy = false,
 	tag = "0.1.8",
 	dependencies = {
@@ -12,38 +12,61 @@ return {
 		},
 	},
 	config = function()
+		vim.api.nvim_set_hl(0, "TelescopePromptBorder", { fg = "#928374" })
+		vim.api.nvim_set_hl(0, "TelescopePreviewBorder", { fg = "#928374" })
+		vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { fg = "#928374" })
+
 		require("telescope").load_extension("fzf")
 		require("telescope").setup({
 			defaults = {
-				-- preview = false,
-        file_ignore_patterns = { "^storage/", "^public/", "^public_html/", "^node_modules/", "^assets/", "^database/migrations/", "^bootstrap/", "^vendor/", "^android/", "^ios/", "^neo4j/" },
+				file_ignore_patterns = {
+					"^storage/",
+					"^public/",
+					"^public_html/",
+					"^node_modules/",
+					"^assets/",
+					"^database/migrations/",
+					"^bootstrap/",
+					"^vendor/",
+					"^android/",
+					"^ios/",
+					"^neo4j/",
+					"^test/",
+				},
 				mappings = {
 					n = {
-						["<A-c>"] = require("telescope.actions").close,
+						["q"] = require("telescope.actions").close,
 						["<C-d>"] = require("telescope.actions").delete_buffer,
 					},
+					i = {
+						["<A-c>"] = require("telescope.actions").close,
+					},
 				},
-        layout_config = {
-          width = 184,
-          height = 45,
-        },
-			},
-			pickers = {
-				find_files = {
-					preview = false,
+				layout_config = {
+					width = 190,
+					height = 45,
 				},
 			},
+			-- pickers = {
+			-- 	find_files = {
+			-- 		preview = false,
+			-- 	},
+			-- },
 			extensions = {
-				fzf = {},
+				fzf = {
+					override_generic_sorter = true,
+					override_file_sorter = true,
+					case_mode = "smart_case",
+				},
 			},
 		})
 	end,
 	keys = {
-		-- { mode = "n", "<A-f>", "<cmd>lua require('telescope.builtin').find_files({ preview = false })<cr>" },
+		{ mode = "n", "<A-f>", "<cmd>lua require('telescope.builtin').find_files({ preview = false })<cr>" },
 		-- { mode = { "n", "t" }, "<A-b>", "<cmd>lua require('telescope.builtin').buffers()<cr>" },
-		-- { mode = "n", "<A-v>", "<cmd>lua require('telescope.builtin').live_grep()<cr>" },
-		-- { mode = "n", "<A-/>", "<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<cr>" },
-		-- { mode = "n", "<space><space>", "<cmd>lua require('telescope.builtin').resume()<cr>" },
+		{ mode = "n", "<A-g>", "<cmd>lua require('telescope.builtin').live_grep()<cr>" },
+		{ mode = "n", "<A-/>", "<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<cr>" },
+		{ mode = "n", "<space><space>", "<cmd>lua require('telescope.builtin').resume()<cr>" },
 		-- { mode = "n", "<A-u>", "<cmd>Telescope undo<cr>" },
 	},
 }

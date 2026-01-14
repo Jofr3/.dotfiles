e60c72810756bc14af8073580c4b9a1f4e9fc3a5
Jofr3 <jofrescari@gmail.com>
2026-01-14 14:58:47 +0100

    mcp

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -30,7 +30,7 @@ alias n="nvim"
 alias ff="fastfetch"
 alias b="btop"
 alias op="opencode"
-alias cc="bunx -y @anthropic-ai/claude-code"
+alias cc="bunx -y @anthropic-ai/claude-code --chrome"
 
 alias ..="cd .."
 

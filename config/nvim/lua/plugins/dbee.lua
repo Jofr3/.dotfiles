0b006a7774bb34b2ab8d621bd201de9b26f2344f
Jofr3 <jofrescari@gmail.com>
2026-07-21 14:25:29 +0200

    asd

--- config/nvim/lua/plugins/dbee.lua
+++ config/nvim/lua/plugins/dbee.lua
@@ -2,6 +2,14 @@ require("dbee").setup({
   drawer = {
     disable_help = true,
     disable_candies = true,
+    mappings = {
+      { key = "<CR>", mode = "n", action = "toggle" },
+    }
+  },
+  editor = {
+    mappings = {
+      { key = "<A-CR>", mode = "n", action = "run_under_cursor" },
+    }
   },
   call_log = {
     disable_candies = true,
